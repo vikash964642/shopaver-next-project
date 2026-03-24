7d76888753e6a3e59090b7bd550e3d2d87bd335c
Dilshad-zyro12 <166795267+Dilshad-zyro12@users.noreply.github.com>
2026-03-24 12:43:58 +0530

    swiper set in RetailBusinessCard

--- src/app/service/business-section/RetailBusinessCard.jsx
+++ src/app/service/business-section/RetailBusinessCard.jsx
@@ -1,107 +1,19 @@
 
-
-// "use client";
-// import { useState } from "react";
-// import Image from "next/image";
-
-// function RetailBusinessCard({ onePlatform = [] }) {
-   
-//   const [showAll, setShowAll] = useState(false);
-
-//   const filteredData = onePlatform.filter(
-//     (item) => item.heading && item.description
-//   );
-// // const baseImageURL="https://media-shopaver-uat.s3.amazonaws.com"
-
-//   return (
-//     <section className="bg-[#F9F4FF] mt-[100px] py-[41px] lg:pt-[68px] lg:pb-[81px]">
-//       <div className="max-w-5xl px-[22px] xl:px-0 mx-auto">
-//         <h2 className="text-[#5801B7] text-center text-[26px] lg:text-[30px] font-semibold">
-//           One platform to Run Every type of Retail Business
-//         </h2>
-
-//         <div className="grid grid-cols-1 sm:grid-cols-2 gap-[10.5px] h-full lg:gap-[17.57px] mt-10">
-//           {filteredData.map((item, index) => (
-//             <div
-//               key={index}
-//               className={`
-//                 ${!showAll && index >= 2 ? "hidden sm:block" : "block"}
-//                 ${showAll && index >= 2 ? "animate-fadeInUp" : ""}
-//               `}
-//             >
-//               <Card item={item} />
-//             </div>
-//           ))}
-//         </div>
-
-//         {!showAll && filteredData.length > 2 && (
-//           <div className="flex justify-center mt-6 sm:hidden">
-//             <div
-//               onClick={() => setShowAll(true)}
-//               className="flex justify-center items-center gap-[5px] w-[111px] h-11 rounded-xl bg-[#5801B7]"
-//             >
-//               <p className="text-white text-[16px] font-medium">More</p>
-//             </div>
-//           </div>
-//         )}
-//       </div>
-//     </section>
-//   );
-// }
-
-// /* Card Component */
-// function Card({ item }) {
-//   const BASE_IMAGE_URL = "https://media-shopaver-uat.s3.amazonaws.com";
-  
-//   return (
-//     <div className=" flex items-center  h-full gap-[14.5px] lg:gap-[35px] bg-white rounded-[25.47px] border border-[#EBD9FF] p-4">
-//       <div className="flex flex-col w-[50%]">
-//         <p className="text-[#5801B7] text-[18px] font-medium break-words">
-//           {item.heading}
-//         </p>
-
-//         <p className="pt-[5px] text-[#525252] text-[12px] break-words">
-//           {item.description}
-//         </p>
-
-//         <div className="mt-[20px] rounded-[30px] bg-[#E6D2FF] w-[90px] h-[27px] flex justify-center items-center">
-//           <p className="text-[#5801B7] text-[10px]">Read More</p>
-//         </div>
-//       </div>
-
-//   <div className="w-[50%]">
-      
-//         <Image
-//           // src={ "/landingPage/RetailBusinessCardImg5.webp"}
-//           src={
-//                           item.image
-//                             ? `${BASE_IMAGE_URL}${item.image}`
-//                             : "/landingPage/BusinessSolutionSliderImg6.webp"
-//                         }
-//           alt={item.heading}
-//           width={218}
-//           height={227}
-//           className=" w-[147px] h-[153px] lg:w-[218px] lg:h-[227px]"
-//         />
-//   </div>
-      
-//     </div>
-//   );
-// }
-
-// export default RetailBusinessCard;
-
-
-
 "use client";
 
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-function RetailBusinessCard({ onePlatform = [] }) {
-  const [showAll, setShowAll] = useState(false);
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
 
+function RetailBusinessCard({ onePlatform = [] }) {
+ const [visibleCount, setVisibleCount] = useState(2);
+const [isBeginning, setIsBeginning] = useState(true);
+const [isEnd, setIsEnd] = useState(false);
   const filteredData = onePlatform.filter(
     (item) => item.heading && item.description
   );
@@ -118,48 +30,71 @@ function RetailBusinessCard({ onePlatform = [] }) {
         </h2>
 
         {/* ================= MOBILE ================= */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-[10.5px] lg:hidden mt-10">
-          {filteredData.map((item, index) => (
-            <div
-              key={index}
-              className={`${
-                !showAll && index >= 2 ? "hidden sm:block" : "block"
-              }`}
-            >
-              <Card item={item} />
-            </div>
-          ))}
+        <div   className={`grid grid-cols-1 sm:grid-cols-2 gap-[10.5px] lg:hidden mt-10 ${
+    filteredData.length === 1 ? "justify-items-center" : ""
+  }`}>
+          {filteredData.slice(0, visibleCount).map((item, index) => (
+    <div key={index}>
+      <Card item={item} />
+    </div>
+  ))}
         </div>
 
         {/* SHOW MORE BUTTON */}
-        {!showAll && filteredData.length > 2 && (
-          <div className="flex justify-center mt-6 sm:hidden">
-            <div
-              onClick={() => setShowAll(true)}
-              className="flex justify-center items-center gap-[5px] w-[111px] h-11 rounded-xl bg-[#5801B7]"
-            >
-              <p className="text-white text-[16px] font-medium">More</p>
-            </div>
-          </div>
-        )}
-
-        {/* ================= DESKTOP GRID ================= */}
-        <div className="hidden lg:flex mt-10 justify-center">
-          <div
-            className={`grid gap-[17.57px] ${
-              filteredData.length === 1
-                ? "grid-cols-1 justify-items-center"
-                : "grid-cols-2"
-            }`}
+      {visibleCount < filteredData.length && (
+  <div className="flex justify-center mt-6 lg:hidden">
+    <div
+      onClick={() => setVisibleCount(prev => prev + 2)}
+      className="flex justify-center items-center gap-[5px] w-[111px] h-11 rounded-xl bg-[#5801B7] cursor-pointer"
+    >
+      <p className="text-white text-[16px] font-medium">View More</p>
+    </div>
+  </div>
+)}
+    <div className="hidden lg:block mt-10">
+          <Swiper
+          onSlideChange={(swiper) => {
+  setIsBeginning(swiper.isBeginning);
+  setIsEnd(swiper.isEnd);
+}}
+            modules={[Navigation]}
+            spaceBetween={20}
+            slidesPerView={2}
+            loop={false}
+            navigation={{
+    nextEl: ".custom-next",
+    prevEl: ".custom-prev",
+  }}
           >
             {filteredData.map((item, index) => (
-              <div key={index} className="w-[500px]">
-                <Card item={item} />
-              </div>
+              <SwiperSlide key={index}>
+                <div className="w-full max-w-[500px] mx-auto">
+                  <Card item={item} />
+                </div>
+              </SwiperSlide>
             ))}
-          </div>
+          </Swiper>
+                 <div className="flex justify-center gap-[12.75px] mt-[10px] lg:mt-[40px]">
+
+        <div
+           
+           className={`custom-prev h-[28px] w-[28px] lg:h-[36.36px] lg:w-[36.36px] rounded-full border-2 border-[#C4C4C4] text-[#C4C4C4]  text-[15px] lg:text-[18px]  flex justify-center items-center ${
+    isBeginning ? "cursor-not-allowed" : "cursor-pointer hover:border-[#5801B7] hover:text-[#5801B7]"
+  }`}
+         >
+           <i className="fa-solid fa-angle-left"></i>
+         </div>
+         <div
+        
+           className={`custom-next w-[28px] h-[28px] lg:h-[36.36px] lg:w-[36.36px] rounded-full border-2 border-[#C4C4C4] text-[#C4C4C4]  text-[15px] lg:text-[18px] flex justify-center items-center ${
+    isEnd ? "cursor-not-allowed" : "cursor-pointer hover:border-[#5801B7] hover:text-[#5801B7]"
+  }`}
+        >
+           <i className="fa-solid fa-angle-right"></i>
         </div>
 
+      </div>
+        </div>
       </div>
     </section>
   );
@@ -177,7 +112,7 @@ function Card({ item }) {
           {item.heading}
         </p>
 
-        <p className="pt-[5px] text-[#525252] text-[12px] break-words">
+        <p className="pt-[5px] text-[#525252] text-[10px] md:text-[12px] break-words">
           {item.description}
         </p>
 
@@ -189,7 +124,7 @@ function Card({ item }) {
         </Link>
       </div>
 
-      <div className="w-[50%]">
+      <div className="w-[50%] flex justify-end items-center">
         <Image
           src={
             item.image
@@ -199,7 +134,7 @@ function Card({ item }) {
           alt={item.heading}
           width={218}
           height={227}
-          className="w-[147px] h-[153px] lg:w-[218px] lg:h-[227px]"
+          className="max-[375px]:h-auto max-[375px]:w-full w-[147px] h-[153px] lg:w-[218px] lg:h-[227px]"
         />
       </div>
 
