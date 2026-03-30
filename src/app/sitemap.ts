b233f6bbe2ba96c3f8e5b9f5a291c91578b61f2f
vikash964642 <vikash.kumar@myzyro.com>
2026-03-30 18:18:57 +0530

    add gtm tag

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,4 @@
-// import { MetadataRoute } from "next";
 
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const res = await fetch(
-//     `${process.env.NEXT_PUBLIC_API_URL}/landingpage/all`,
-//     {
-//       next: { revalidate: 300 }, // revalidate every 5 mins
-//     }
-//   );
-
-//   const pages = await res.json();
-
-//   const servicePages = pages
-//     .filter((page: any) => page.isActive)
-//     .map((page: any) => ({
-//       url: `https://www.shopaver.com/service/${page.slug}`,
-//       lastModified: new Date(page.updatedDate),
-//       changeFrequency: "weekly",
-//       priority: 0.8,
-//     }));
-
-//   return [
-//     {
-//       url: "https://www.shopaver.com",
-//       lastModified: new Date(),
-//       changeFrequency: "weekly",
-//       priority: 1,
-//     },
-//     ...servicePages,
-//   ];
-// }
 
 import { MetadataRoute } from "next";
 
