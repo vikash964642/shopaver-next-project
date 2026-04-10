7ed6e7e516aa3c6e54df04a4397e7744edefb22b
Dilshad-zyro12 <166795267+Dilshad-zyro12@users.noreply.github.com>
2026-04-10 12:39:52 +0530

    7 days to 14 days changed

--- src/app/service/free-trial-banner/FreeTrialBanner.jsx
+++ src/app/service/free-trial-banner/FreeTrialBanner.jsx
@@ -7,7 +7,7 @@ function FreeTrialBanner() {
         <div className='flex w-full gap-[38px] md:gap-0 flex-col md:flex-row  md:h-[261px] rounded-[17.21px] bg-[#5801B7]'>
 <div className='pl-[13px] pt-[15px] md:pt-0 flex flex-col  md:justify-center md:pl-[20px] lg:pl-[160px] md:w-1/2'>
   <h2 className='w-[165px] sm:w-[200px] md:w-[300px] text-[#D7B3FF] text-[20px] sm:text-[25px] lg::text-[30px] font-semibold'>Get started with Shopaver today</h2> 
-  <p className='w-[240px] md:w-[300px] pt-[11px] md:pt-[8px] text-[#FFF] text-[12px]'>Try Shopaver free for 7 days—you'll have access to every feature.
+  <p className='w-[240px] md:w-[300px] pt-[11px] md:pt-[8px] text-[#FFF] text-[12px]'>Try Shopaver free for 14 days—you'll have access to every feature.
 Cancel any time.</p> 
 <Link href="https://app.shopaver.com/Sign-up" className='mt-[13.5px] md:mt-[17.2px] bg-[#FFF] flex justify-center items-center w-[150px] h-[35px] md:w-[125px] md:h-[31px] rounded-[4.303px] text-[#5801B7] text-[14px] md:text-[12px] font-medium'>Start your free trial</Link>
 </div>
